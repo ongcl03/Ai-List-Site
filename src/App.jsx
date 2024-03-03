dfe1206c157fb7b5a3b742d015a7895bc1b341f2
CL <ongchangle03@gmail.com>
2024-03-03 12:35:59 +0800

    Deployed site to vercel and add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { sources } from "./constants";
 import Footer from "./components/Footer";
 import PageNotFound from "./components/PageNotFound";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   const [filteredSources, setFilteredSources] = useState(sources);
@@ -15,22 +16,25 @@ const App = () => {
   };
 
   return (
-    <div className="bg-[#1a1a2e]">
-      <Title />
-      <SearchBar onSearch={handleSearch} />
-      {filteredSources.length > 0 ? (
-        filteredSources.map((source) => (
-          <Category
-            key={source.title}
-            title={source.title}
-            tools={source.tools}
-          />
-        ))
-      ) : (
-        <PageNotFound />
-      )}
-      <Footer />
-    </div>
+    <>
+      <div className="bg-[#1a1a2e]">
+        <Title />
+        <SearchBar onSearch={handleSearch} />
+        {filteredSources.length > 0 ? (
+          filteredSources.map((source) => (
+            <Category
+              key={source.title}
+              title={source.title}
+              tools={source.tools}
+            />
+          ))
+        ) : (
+          <PageNotFound />
+        )}
+        <Footer />
+      </div>
+      <Analytics />
+    </>
   );
 };
 
